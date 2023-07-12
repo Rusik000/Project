cf951ad6980548f774706b57a4bda27aa650b00a
Ruslan <mustafaevv.2000@gmail.com>
2023-07-12 17:07:57 +0400

    product-list ve product componenti is added

--- ng-app/src/app/product-list/product-list.component.ts
+++ ng-app/src/app/product-list/product-list.component.ts
@@ -12,4 +12,40 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  product = {
+    id: 1,
+    name: "IPhone 15",
+    price: 20000,
+    imageUrl: "1.jpeg",
+    description: "Zor Telefon",
+    isActive: true
+  }
+
+  productList = ["IPhone 14", "IPhone 15", "IPhone 16", "IPhone 17"]
+  products = [
+    {
+      id: 1,
+      name: "IPhone 14",
+      price: 20000,
+      imageUrl: "1.jpeg",
+      description: "Zor Telefon",
+      isActive: true
+    },
+    {
+      id: 2,
+      name: "IPhone 15",
+      price: 20000,
+      imageUrl: "1.jpeg",
+      description: "Zor Telefon",
+      isActive: true
+    },
+    {
+      id: 3,
+      name: "IPhone 16",
+      price: 20000,
+      imageUrl: "1.jpeg",
+      description: "Zor Telefon",
+      isActive: true
+    }
+  ]
 }
